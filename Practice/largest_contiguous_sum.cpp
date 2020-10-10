15cb9a5bcc5d40c3d9cee119bf74b4f823a63ba8
Pawandeep Singh <pawandeep2155@gmail.com>
2020-10-10 14:04:54 +0530

    Larges Contiguous Sum with Index

--- Practice/largest_contiguous_sum.cpp
+++ Practice/largest_contiguous_sum.cpp
@@ -6,14 +6,22 @@
 using namespace std;
 
 int main(){
-    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int arr[] = {-2, -3, -1, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
     int sum = 0, max_sum = INT_MIN;
+    int current_start = 0, start, end;
     for(int i = 0; i < n; i++){
         sum += arr[i];
-        max_sum = max(sum, max_sum);
-        sum = max(sum, 0);
+        if(sum > max_sum){
+            max_sum = sum;
+            start = current_start;
+            end = i;
+        }
+        if(sum < 0){
+            sum = 0;
+            current_start = i + 1;
+        }
     }
-    cout<<"Max contiguous sum "<<max_sum<<endl;
+    cout<<"Max contiguous sum "<<max_sum<<" Start "<<start<<" End "<<end<<endl;
     return 0;
 }
